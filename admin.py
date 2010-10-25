8f1002883d116a06dd7b9b4176ac23ca42b23573
Uri Sharf <uri.sharf@gmail.com>
2010-10-25 22:40:39 +0200

    Finished admin

--- admin.py
+++ admin.py
@@ -14,6 +14,20 @@ import fix_path
 import config
 import static
 
+def slugify(s):
+	return re.sub('[^a-zA-Z0-9-]+', '-', s).strip('-')
+	
+def format_post_path(post, num):
+	slug = slugify(post.title)
+	if num > 0:
+		slug += "-" + str(num)
+	return config.post_path_format % {
+		'slug': slug,
+		'year': post.published.year,
+		'month': post.published.month,
+		'day': post.published.day,
+	}
+	
 def render_template(template_name, template_vals=None, theme=None):
   template_path = os.path.join("themes", theme or config.theme, template_name)
   return template.render(template_path, template_vals or {})
@@ -25,14 +39,28 @@ class BlogPost(db.Model):
   published = db.DateTimeProperty(auto_now_add=True)
   updated = db.DateTimeProperty(auto_now=True)
   
-  def render(sef)::
+  def render(self):
     template_vals = {
       'config': config,
       'post': self,
     }
     return render_template("post.html", template_vals)
+
+  def publish(self):
+    rendered = self.render()
+    if not self.path:
+      num = 0
+      content = None
+      while not content:
+        path = format_post_path(self, num)
+        content = static.add(path, rendered, "text/html")
+        num += 1
+        self.path = path
+        self.put()
+      else:
+        static.set(self.path, rendered, "text/html")
   
-def PostForm(djangoforms.ModelForm):
+class PostForm(djangoforms.ModelForm):
   class Meta:
     model = BlogPost
     exclude = ['path', 'published', 'updated']
@@ -49,7 +77,7 @@ class PostHandler(webapp.RequestHandler):
     self.render_form(PostForm())
     
   def post(self):
-    form = PostForm(date=self.request.POST)
+    form = PostForm(data=self.request.POST)
     if form.is_valid():
       post = form.save(commit=False)
       post.publish()
@@ -61,10 +89,8 @@ application = webapp.WSGIApplication([
   ('/admin/newpost', PostHandler),
 ])
 
-
 def main():
   run_wsgi_app(application)
-
 
 if __name__ == '__main__':
   main()      
